053d7b388c7d754650027826b92d4bf051fb771f
deepanshu <deepanshu.dabas@gmail.com>
2017-10-09 04:03:36 +0530

    026 solution

---
+++ 026/026.py
@@ -0,0 +1,16 @@
+#This is a python3 script that is can autmate the clicking process
+#It clicks for 250 times after every .6 seconds at any postion and displays a message while doing so.
+
+
+import pyautogui
+import time
+
+x,y = pyautogui.position()
+pyautogui.rightClick(x,y)
+click=250
+while(click--):
+	x,y = pyautogui.position()
+	p=15
+	pyautogui.click(x,y)
+	print("we are clicking")
+	time.sleep(.6)	
